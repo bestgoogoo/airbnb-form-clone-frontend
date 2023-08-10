f63c9accd0e07202f635f7059385d47d58d2998d
bestgoogoo <junta9996@gmail.com>
2023-08-10 16:28:50 +0900

    Header form creation in Root 1. For using free icons, install react-icons 2. Styled chakra-ui syntax

--- src/conponents/Root.tsx
+++ src/conponents/Root.tsx
@@ -1,10 +1,30 @@
+import { Box, Button, HStack, IconButton } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { FaAirbnb, FaMoon } from "react-icons/fa";
 
 export default function Root() {
   return (
-    <h1>
-      Root
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        py={5}
+        px={10}
+        borderBottomWidth={1}
+      >
+        <Box color="red.500">
+          <FaAirbnb size={"48"} />
+        </Box>
+        <HStack spacing={2}>
+          <IconButton
+            variant={"ghost"}
+            aria-label="Toggle dark mode"
+            icon={<FaMoon />}
+          />
+          <Button>Log In</Button>
+          <Button colorScheme="red">Sign Up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
