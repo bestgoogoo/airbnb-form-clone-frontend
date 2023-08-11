09e1e84cc1676bf7ddea98026299edc85eab29dc
bestgoogoo <junta9996@gmail.com>
2023-08-11 18:38:05 +0900

    Fetch of TanStack Query 1. Install and setting QueryClientProvider of TanStack Query 2. Using useQuery hook of TanStack Query

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./router";
 import theme from "./components/theme";
+
+const client = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMod} />
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMod} />
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { Grid } from "@chakra-ui/react";
+import { getRooms } from "../api";
 import RoomSkeleton from "../components/RoomSkeleton";
 import Room from "../components/Room";
 
@@ -22,17 +23,8 @@ interface IRoom {
 }
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [rooms, setRooms] = useState<IRoom[]>([]);
-  const fetchRooms = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
-    const json = await response.json();
-    setRooms(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    fetchRooms();
-  }, []);
+  // useQuery(["key"], func) -> data is stored in cashe by key name
+  const { isLoading, data } = useQuery<IRoom[]>(["rooms"], getRooms);
   return (
     <Grid
       mt={10}
@@ -62,7 +54,7 @@ export default function Home() {
           <RoomSkeleton />
         </>
       ) : null}
-      {rooms.map((room) => (
+      {data?.map((room) => (
         <Room
           imageUrl={room.photos[0].file}
           name={room.name}
