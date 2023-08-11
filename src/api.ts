26523d759a5bb4876c7b97e1b78f21d42e19f14e
bestgoogoo <junta9996@gmail.com>
2023-08-11 18:44:07 +0900

    About axois - axios is like adapter of fetch - Let's use it because it reduces the chance of mistakes

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,16 @@
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+import axios from "axios";
 
-export async function getRooms() {
-  const response = await fetch(`${BASE_URL}/rooms/`);
-  const json = await response.json();
-  return json;
-}
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1",
+});
+
+export const getRooms = () =>
+  instance.get("rooms").then((response) => response.data);
+
+// Same with upper const
+
+// export function getRooms() {
+//   const response = await instance.get("rooms/");
+
+//   return response.data;
+// }
