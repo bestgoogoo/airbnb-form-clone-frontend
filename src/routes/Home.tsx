af7032cfdd2b6fba5e2c027748ad5e35c2af15b0
bestgoogoo <junta9996@gmail.com>
2023-08-11 15:07:34 +0900

    Grid of rooms list form in Home route - With Box, Button, Grid,  Image, Text, VStack, HStack - and many many many many props

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,67 @@
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FaRegHeart, FaStar } from "react-icons/fa";
+
 export default function Home() {
-  return <span>Home</span>;
+  return (
+    <Grid
+      mt={10}
+      px={40}
+      columnGap={5}
+      rowGap={10}
+      templateColumns="repeat(5, 1fr)"
+    >
+      {[
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      ].map((index) => (
+        <VStack key={index} alignItems="flex-start" spacing={1}>
+          <Box
+            position="relative"
+            mb={2}
+            overflow="hidden"
+            rounded="lg"
+            h={220}
+          >
+            <Image src="https://a0.muscache.com/im/pictures/6b3f1fe9-833f-449e-aacb-c2e9a2466ce7.jpg?im_w=720" />
+            <Button
+              variant="unstyled"
+              position="absolute"
+              top={0}
+              right={0}
+              color="white"
+              cursor="pointer"
+            >
+              <FaRegHeart />
+            </Button>
+          </Box>
+          <Box>
+            <Grid gap={2} templateColumns="4fr 1fr">
+              <Text as="b" noOfLines={1} fontSize="xs">
+                Brooklin,NewYork,United State
+              </Text>
+              <HStack spacing={1}>
+                <FaStar size={10} />
+                <Text fontSize="xs">5.0</Text>
+              </HStack>
+            </Grid>
+            <Text fontSize="xs" color="gray.600">
+              Brooklin, New York
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="xs" color="gray.600">
+              <Text as="b">$72</Text> / night
+            </Text>
+          </Box>
+        </VStack>
+      ))}
+    </Grid>
+  );
 }
