703580da9a99b25206289d45fad33b7d90f8e002
bestgoogoo <junta9996@gmail.com>
2023-08-14 14:30:31 +0900

    Axios Error Fix - One line const code has error, check later

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: "http://127.0.0.1:8000/api/v1/",
 });
 
-export const getRooms = () =>
-  instance.get("rooms").then((response) => response.data);
-
-// Same with upper const
-
-// export function getRooms() {
-//   const response = await instance.get("rooms/");
-
-//   return response.data;
-// }
+export async function getRooms() {
+  const response = await instance.get("rooms/");
+  return response.data;
+}
+// Same with upper const but it has error
+// export const getRooms = async () =>
+//   await instance.get("rooms").then((response) => response.data);
