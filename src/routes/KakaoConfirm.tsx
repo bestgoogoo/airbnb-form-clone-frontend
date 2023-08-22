5a3f40b31e0e5f3bcbb6dd3a298ffc59c2fba5b6
bestgoogoo <junta9996@gmail.com>
2023-08-22 15:02:30 +0900

    Kakao Log In Refactoring - Using Mutation

--- src/routes/KakaoConfirm.tsx
+++ src/routes/KakaoConfirm.tsx
@@ -1,5 +1,5 @@
 import { Heading, Spinner, Text, VStack, useToast } from "@chakra-ui/react";
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { kakaoLogIn } from "../api";
@@ -9,21 +9,23 @@ export default function KakaoConfirm() {
   const toast = useToast();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const mutation = useMutation(kakaoLogIn, {
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Welcome!😁",
+        description: "Happy to have you back!",
+        position: "top",
+      });
+      queryClient.refetchQueries(["me"]);
+      navigate("/");
+    },
+  });
   const confirmLogin = async () => {
     const params = new URLSearchParams(search);
     const code = params.get("code");
     if (code) {
-      const status = await kakaoLogIn(code);
-      if (status === 200) {
-        toast({
-          status: "success",
-          title: "Welcome!😁",
-          description: "Happy to have you back!",
-          position: "top",
-        });
-        queryClient.refetchQueries(["me"]);
-        navigate("/");
-      }
+      mutation.mutate(code);
     }
   };
   useEffect(() => {
