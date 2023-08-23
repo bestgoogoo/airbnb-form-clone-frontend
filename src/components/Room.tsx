6d6f9e02585673c330434752c02999deeb7cd7e5
bestgoogoo <junta9996@gmail.com>
2023-08-23 18:25:00 +0900

    Upload Photo Setting from Home - In the room list, the icon of wishlist(heart) is change to a camera, and when the owner clicks it, it moves to the photo upload page. - The reason for putting 'preventDefault()' is that <Button /> is inside <Link />, so props of <Link /> is default.

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -8,8 +8,9 @@ import {
   VStack,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { FaRegHeart, FaStar } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React from "react";
+import { FaCamera, FaRegHeart, FaStar } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 
 interface IRoomProps {
   pk: number;
@@ -19,6 +20,7 @@ interface IRoomProps {
   city: string;
   country: string;
   price: number;
+  isOwner: boolean;
 }
 
 export default function Room({
@@ -29,8 +31,14 @@ export default function Room({
   city,
   country,
   price,
+  isOwner,
 }: IRoomProps) {
   const grayFont = useColorModeValue("gray.600", "gray.400");
+  const navigate = useNavigate();
+  const onCameraClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    navigate(`/rooms/${pk}/photos`);
+  };
   return (
     <Link to={`/rooms/${pk}`}>
       <VStack alignItems="flex-start">
@@ -49,11 +57,12 @@ export default function Room({
           <Button
             variant="unstyled"
             position="absolute"
+            onClick={onCameraClick}
             top={0}
             right={0}
             color="white"
           >
-            <FaRegHeart />
+            {isOwner ? <FaCamera size="20px" /> : <FaRegHeart size="20px" />}
           </Button>
         </Box>
         <Box>
